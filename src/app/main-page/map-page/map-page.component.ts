2b88b5661ae01873f85af913ae3ad6427d417681
Georgy Saukov <georgy.saukov@sap.com>
2024-02-28 20:36:56 +0100

    GHSL client baseline implementation

--- src/app/main-page/map-page/map-page.component.ts
+++ src/app/main-page/map-page/map-page.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {MapService} from "../../services/map.service";
+import Map from 'ol/Map';
 
 @Component({
   selector: 'app-map-page',
   templateUrl: './map-page.component.html',
   styleUrl: './map-page.component.css'
 })
-export class MapPageComponent {
+export class MapPageComponent implements OnInit {
 
+  private _map!: Map;
+
+  constructor(private mapService: MapService) {
+  }
+
+  ngOnInit(): void {
+    if (!this._map) {
+      this.mapService.buildMap().subscribe(map => this._map = map);
+    }
+  }
+
+  get map(): Map {
+    return this._map;
+  }
 }
---
+++ src/app/services/map.service.ts
@@ -0,0 +1,50 @@
+import {Injectable} from '@angular/core';
+import Map from "ol/Map";
+import View from "ol/View";
+import {fromLonLat} from "ol/proj";
+import TileLayer from "ol/layer/Tile";
+import OSM from "ol/source/OSM";
+import {Observable} from "rxjs";
+import {ScaleLine, defaults} from "ol/control";
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MapService {
+
+  private map!: Map;
+
+  constructor() {
+  }
+
+  buildMap(): Observable<Map> {
+    return new Observable((observer) => {
+      const map = new Map({
+        controls: defaults().extend([new ScaleLine({
+          units: 'metric',
+        })]),
+        view: new View({
+          center: fromLonLat([69.2787079, 41.3123363], 'EPSG:3857'),
+          zoom: 12,
+        }),
+        layers: [
+          new TileLayer({
+            source: new OSM({
+              attributions: '<a href="https://gsaukov.netlify.app/" target="_blank" style="color:blue;">Georgy Saukov</a> &copy; ' +
+                '<a href="https://www.openstreetmap.org/copyright" target="_blank" style="color:blue;">OpenStreetMap</a> contributors',
+            }),
+          }),
+        ],
+        target: 'ol-map',
+      });
+      observer.next(this.map = map);
+      observer.complete();
+    });
+  }
+
+  getMap(): Map {
+    return this.map;
+  }
+
+}
