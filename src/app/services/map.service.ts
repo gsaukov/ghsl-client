bfe5c54625cbd5e97d603abf975ed5ab3a6b1d9f
Georgy Saukov <georgy.saukov@sap.com>
2024-02-29 16:20:11 +0100

    GHSL client data preprocessing

--- src/app/services/map.service.ts
+++ src/app/services/map.service.ts
@@ -10,6 +10,10 @@ import GeoJSON from 'ol/format/GeoJSON';
 import Layer from 'ol/layer/Layer';
 import VectorSource from 'ol/source/Vector';
 import WebGLVectorLayerRenderer from 'ol/renderer/webgl/VectorLayer';
+import {Fill, Stroke, Style} from "ol/style";
+import {Feature} from "ol";
+import { Polygon } from 'ol/geom';
+import {Vector} from "ol/layer";
 
 const style = {
   'stroke-color': ['*', ['get', 'COLOR'], [220, 220, 220]],
@@ -81,4 +85,85 @@ export class MapService {
     return vectorLayer
   }
 
+  loadJSON(response:any) {
+    var jsonObject = JSON.parse(response);
+
+    const top = jsonObject.metaData.topLeftCorner[0]; // lon
+    const left = jsonObject.metaData.topLeftCorner[1]; // lat
+    const bottom = jsonObject.metaData.bottomRightCorner[0]; // lon
+    const right = jsonObject.metaData.bottomRightCorner[1]; // lat
+    const height = jsonObject.metaData.pixelHeightDegrees;
+    const width = jsonObject.metaData.pixelWidthDegrees;
+    const rows = jsonObject.metaData.areaWidth
+    const cols = jsonObject.metaData.areaHeight
+    const data = jsonObject.data.res
+    const squares = []
+    let olCenter
+
+    const center = [top, left];
+    olCenter = fromLonLat(center);
+
+    for (let i = 0; i < 1200; i++) {
+      for (let j = 0; j < 1200; j++) {
+        const pixel = data[(i * cols) + j]
+        if (pixel > 0) {
+          let verticalPos = top + (j * height);
+          let hotizontalPos = left - (i * width);
+          const leftTop = [verticalPos, hotizontalPos];
+
+          // Create a square feature
+          const squareFeature = new Feature({
+            geometry: new Polygon([[
+              fromLonLat([leftTop[0], leftTop[1]]),
+              fromLonLat([leftTop[0], leftTop[1] - height]),
+              fromLonLat([leftTop[0] + width, leftTop[1] - height]),
+              fromLonLat([leftTop[0] + width, leftTop[1]]),
+              fromLonLat([leftTop[0], leftTop[1]]),
+            ]])
+          });
+          squareFeature.setStyle(this.getStyle(pixel));
+          squares.push(squareFeature)
+        }
+      }
+    }
+
+    // Create a vector layer to display the square
+    const vectorLayer = new Vector({
+      source: new VectorSource({
+        features: squares
+      }),
+    });
+  }
+
+  getStyle(num:number) {
+    let color
+
+    if(num < 10) {
+      color = 'rgba(0, 0, 255, 0.05)'
+    } else if (num < 50) {
+      color = 'rgba(0, 0, 255, 0.1)'
+    } else if (num < 100) {
+      color = 'rgba(0, 0, 255, 0.15)'
+    } else if (num < 200) {
+      color = 'rgba(0, 0, 255, 0.20)'
+    } else if (num < 300) {
+      color = 'rgba(0, 0, 255, 0.25)'
+    } else if (num < 500) {
+      color = 'rgba(0, 0, 255, 0.30)'
+    } else if (num < 1000) {
+      color = 'rgba(0, 0, 255, 0.40)'
+    } else if (num < 5000) {
+      color = 'rgba(0, 0, 255, 0.50)'
+    }else if (num > 5000) {
+      color = 'rgba(0, 0, 255, 0.60)'
+    }
+    return new Style({
+      fill: new Fill({
+        color: color
+      }),
+      stroke: new Stroke({
+        color: 'rgba(0, 0, 0, 0)'
+      })
+    });
+  }
 }
