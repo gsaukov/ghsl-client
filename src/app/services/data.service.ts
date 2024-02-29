b626cf4fb17a283ea3b16d486e1d3708f23a7479
Georgy Saukov <georgy.saukov@sap.com>
2024-02-29 14:32:38 +0100

    GHSL client data service name fix.

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -5,7 +5,7 @@ import {Observable} from "rxjs";
 @Injectable({
   providedIn: 'root'
 })
-export class NominatimService {
+export class DataService {
 
   constructor(private http: HttpClient) {
   }
