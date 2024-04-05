1a952bb5514bf2cfa5babe43ef46da8b01d5f11c
Georgy Saukov <georgy.saukov@sap.com>
2024-04-05 18:40:52 +0200

    GHSL client ol-ext geo image layer service WORKS! still draft.

--- src/app/services/image-ext-layer.service.ts
+++ src/app/services/image-ext-layer.service.ts
@@ -19,27 +19,23 @@ export class ImageExtLayerService {
 
   createImageLayer(map: Map): ImageLayer<ImageSource> {
     const imageMask = [
+      //REORDER IMAGE MASK!
       fromLonLat([19.992083276545834, 49.09958333862941]),
+      fromLonLat([19.992083276545834, 39.099583378875366]),
       fromLonLat([9.992083316153526, 39.099583378875366]),
       fromLonLat([9.992083316153526, 49.09958333862941]),
-      fromLonLat([19.992083276545834, 39.099583378875366]),
       fromLonLat([19.992083276545834, 49.09958333862941]),
     ]
 
-    const x= 16.984416292988566
-    const y= 45.29974915412719
+    const x= 14.9920833999
+    const y= 44.09958336
     const sx = 1200;
     const sy = 1200;
-    const xmin= 0
-    const ymin = 0
-    const xmax=1200
-    const ymax =1200
 
     const geoImageSource = new GeoImageSource({
       url: 'https://localhost:4200/assets/GHS_POP_E2025_GLOBE_R2023A_4326_30ss_V1_0_R5_C20.png',
       imageCenter: fromLonLat([x, y]),
-      imageScale: [sx,sy],
-      imageCrop: [xmin,ymin,xmax,ymax],
+      imageScale: [sx, sy],
       imageMask: imageMask,
       // imageRotate: Number($("#rotate").val()*Math.PI/180),
       // projection: 'EPSG:4326',
@@ -50,8 +46,10 @@ export class ImageExtLayerService {
     map.addLayer(geoImgLayer)
     map.setView(new View({
       center: fromLonLat([x, y]),
-      zoom: 8
+      zoom: 6
     }))
+
+    //FINAL IMAGE PROJECTION IS INCORRECTLY STRETCHED!
 
     return geoImgLayer;
   }
