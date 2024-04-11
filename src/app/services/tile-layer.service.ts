fd4bbdebaf958ecc5f9bac2f20fb673c4b09078d
Georgy Saukov <georgy.saukov@sap.com>
2024-04-11 19:01:19 +0200

    GHSL client tile-layer service garbage collection and rewwork to hashmaps.

--- src/app/services/tile-layer.service.ts
+++ src/app/services/tile-layer.service.ts
@@ -1,8 +1,8 @@
 import {Injectable} from '@angular/core';
-import Map from "ol/Map";
+import OlMap from "ol/Map";
 import {transformExtent} from "ol/proj";
 import {Polygon} from "ol/geom";
-import {getBottomLeft, getBottomRight, getTopLeft, getTopRight} from "ol/extent";
+import {getBottomLeft, getBottomRight, getTopLeft, getTopRight} from "ol/extent"
 import {meta30ss} from "./metaData30ss"
 import {ImageLayerService} from "./image-layer.service";
 
@@ -10,6 +10,8 @@ import {ImageLayerService} from "./image-layer.service";
   providedIn: 'root'
 })
 export class TileLayerService {
+
+  public static ID:string = "GHSL_ID";
 
   //check:
   // https://openlayers.org/en/latest/examples/sea-level.html
@@ -19,18 +21,18 @@ export class TileLayerService {
   // LAZY loading: https://stackoverflow.com/questions/77428955/openlayer-on-zooming-map-image-filter-get-only-visible-areas-on-map
 
   polygonsArray: Polygon[]
-  visiblePolygons: Polygon[] = [];
+
+  visiblePolygonsMap: Map<string, Polygon> = new Map ();
 
   constructor(private imageLayerService:ImageLayerService) {
     this.polygonsArray = this.getPolygons()
-    console.log(this.polygonsArray)
   }
 
-  createTileLayer(map: Map) {
+  createTileLayer(map: OlMap) {
     this.mapTiler(map)
   }
 
-  mapTiler (map: Map) {
+  mapTiler (map: OlMap) {
     map.on('moveend', () => {
       const mapExtent =  map.getView().calculateExtent(map.getSize());
       const extent = transformExtent(mapExtent, 'EPSG:3857', 'EPSG:4326');
@@ -40,14 +42,18 @@ export class TileLayerService {
       const br = getBottomRight(extent);
       const mapViewPolygon = new Polygon([[tl, tr, br, bl, tl]]);
 
+      const tempVisiblePolygonsMap: Map<string, Polygon> = new Map ();
       this.polygonsArray.forEach(layerPolygon => {
         const layerPolygonExtent = layerPolygon.getExtent()
         if (mapViewPolygon.intersectsExtent(layerPolygonExtent)) {
-          this.visiblePolygons.push(layerPolygon);
-          const imageTile = this.imageLayerService.createImageLayer(layerPolygon)
-          map.addLayer(imageTile)
+          tempVisiblePolygonsMap.set(layerPolygon.get(TileLayerService.ID), layerPolygon)
         }
       })
+      const toAddVisiblePolygonsMap: Map<string, Polygon> = this.mapDifference(tempVisiblePolygonsMap, this.visiblePolygonsMap)
+      const toDeleteVisiblePolygonsMap: Map<string, Polygon> = this.mapDifference(this.visiblePolygonsMap, tempVisiblePolygonsMap)
+      this.visiblePolygonsMap = new Map([...this.visiblePolygonsMap.entries(), ...toAddVisiblePolygonsMap.entries()])
+      this.deleteFromMap(map, toDeleteVisiblePolygonsMap)
+      this.addPolygonsToMap(map, toAddVisiblePolygonsMap)
     })
   }
 
@@ -59,10 +65,43 @@ export class TileLayerService {
       const bl = meta30ss[key].bottomLeftCorner
       const br = meta30ss[key].bottomRightCorner
       const polygon = new Polygon([[tl, tr, br, bl, tl]]);
-      polygon.set("id", key)
+      polygon.set(TileLayerService.ID, key)
       polygons.push(polygon);
     });
     return polygons
+  }
+
+  deleteFromMap(map:OlMap, polygonsMap: Map<string, Polygon>) {
+    map.getLayers().forEach(layer => {
+      if (layer && polygonsMap.has(layer.get(TileLayerService.ID))) {
+        map.removeLayer(layer);
+      }
+    });
+  }
+
+  addPolygonsToMap(map:OlMap, polygonsMap: Map<string, Polygon>) {
+    polygonsMap.forEach((value, key) => {
+      map.getLayers().forEach(layer => {
+        if (!polygonsMap.has(layer.get(TileLayerService.ID))) { //if we already have layer from map no need to add it again.
+          this.addPolygonToMap(map, value)
+        }
+      });
+    });
+  }
+
+  addPolygonToMap(map:OlMap, polygon:Polygon) {
+    const imageTile = this.imageLayerService.createImageLayer(polygon)
+    map.addLayer(imageTile)
+  }
+
+  mapDifference(map1: Map<string, Polygon>, map2: Map<string, Polygon>): Map<string, Polygon> {
+    const difference = new Map<string, Polygon>();
+    map1.forEach((value, key) => {
+      if (!map2.has(key)) {
+        difference.set(key, value);
+      }
+    });
+    return difference;
   }
 
 }
