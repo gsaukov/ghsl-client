7336b893666dbee6d8fd8c476d374387a8d411c3
Georgy Saukov <georgy.saukov@sap.com>
2024-04-12 18:35:30 +0200

    GHSL client image-layer service fix and ability to load image from polygon or direct URL with extent.

--- src/app/services/image-layer.service.ts
+++ src/app/services/image-layer.service.ts
@@ -5,6 +5,12 @@ import {Projection} from "ol/proj";
 import {Polygon} from "ol/geom";
 import Static from "ol/source/ImageStatic";
 import {TileLayerService} from "./tile-layer.service";
+import {Extent} from "ol/extent";
+import OlMap from "ol/Map";
+
+// An array of numbers representing an extent: [minx, miny, maxx, maxy].
+// let extent = [9.992083316153526, 39.099583378875366, 19.992083276545834, 49.09958333862941]
+// let extent = [9.992083316153526, 29.099583419121316, 19.992083276545834, 59.09958329838346]
 
 @Injectable({
   providedIn: 'root'
@@ -14,14 +20,18 @@ export class ImageLayerService {
   constructor() {
   }
 
-  createImageLayer(polygon: Polygon): ImageLayer<Static> {
-    // An array of numbers representing an extent: [minx, miny, maxx, maxy].
-    // let extent = [9.992083316153526, 39.099583378875366, 19.992083276545834, 49.09958333862941]
+  addImageLayerFromPolygon(map:OlMap, polygon: Polygon) {
     let extent = polygon.getExtent()
+    const url = `https://localhost:4200/assets/${polygon.get(TileLayerService.ID)}.png`
+    return this.addImageLayerFromExtentAndUrl(map, extent, url);
+  }
+
+  addImageLayerFromExtentAndUrl(map:OlMap, extent: Extent, url: string) {
     let projection = new Projection({code:'EPSG:4326'})
     let imageStatic = new ImageStatic({
-      url: `https://localhost:4200/assets/${polygon.get(TileLayerService.ID)}.png`,
+      url: url,
       imageExtent: extent,
+      attributions: 'none',
       projection: projection,
       interpolate: false, // when false displays pixels and do smooth/blurs images on zoom.
     })
@@ -29,10 +39,10 @@ export class ImageLayerService {
     let imageLayer = new ImageLayer({
       source: imageStatic,
       className: 'ghsl-image',
-      opacity: 0.5
+      opacity: 1,
+      visible: true,
+      map: map
     });
-
-    return imageLayer;
   }
 
 }
