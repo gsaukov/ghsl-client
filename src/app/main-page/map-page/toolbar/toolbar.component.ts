ac39f6127942e23bf5a60e02faec44423ec0873e
Georgy Saukov <georgy.saukov@sap.com>
2024-04-29 20:24:20 +0200

    GHSL client toolbar opacity control

--- src/app/main-page/map-page/toolbar/toolbar.component.ts
+++ src/app/main-page/map-page/toolbar/toolbar.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {TileLayerService} from "../../../services/tile-layer.service";
+import {MatSliderDragEvent} from "@angular/material/slider";
 
 @Component({
   selector: 'app-toolbar',
@@ -6,5 +8,14 @@ import { Component } from '@angular/core';
   styleUrl: './toolbar.component.css'
 })
 export class ToolbarComponent {
+
+  constructor(private tileLayerService: TileLayerService) {
+  }
+
+  applyOpacity(event: MatSliderDragEvent) {
+    this.tileLayerService.getVisibleLayers().forEach((value, key) => {
+      value.setOpacity(event.value)
+    });
+  }
 
 }
