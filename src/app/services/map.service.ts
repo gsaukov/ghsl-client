e23d96c759a809889ece7e9704443952f73ab953
Georgy Saukov <georgy.saukov@sap.com>
2024-05-24 18:19:44 +0200

    GHSL client dark mode

--- src/app/services/map.service.ts
+++ src/app/services/map.service.ts
@@ -7,6 +7,8 @@ import OSM from "ol/source/OSM";
 import {mergeMap, Observable, of} from "rxjs";
 import {ScaleLine, defaults} from "ol/control";
 import {TileLayerService} from "./tile-layer.service";
+import Tile from 'ol/layer/Tile';
+import StadiaMaps from 'ol/source/StadiaMaps';
 
 const DEFAULT_COORDINATE: number[] = [10.40041664, 48.77458333]
 const DEFAULT_ZOOM = 8
@@ -43,6 +45,30 @@ export class MapService {
 
   buildMap(): Observable<Map> {
     return new Observable((observer) => {
+      const attributions = '<a href="https://gsaukov.netlify.app/" target="_blank" style="color:blue;">Georgy Saukov</a> ' +
+          '| <a href="https://human-settlement.emergency.copernicus.eu/download.php?ds=pop" target="_blank" style="color:blue;">GHSL Data</a> ' +
+          '&copy; <a href="https://www.openstreetmap.org/copyright" target="_blank" style="color:blue;">OpenStreetMap</a> contributors'
+
+      const defaultLayer = new TileLayer({
+        source: new OSM({
+          attributions: attributions
+        }),
+        visible: true
+      })
+
+      const stadiaMaps = new StadiaMaps({
+        layer: 'alidade_smooth_dark',
+        retina: true,
+        // apiKey: 'OPTIONAL'
+      })
+
+      stadiaMaps.setAttributions(attributions)
+
+      const darkLayer = new TileLayer({
+        source: stadiaMaps,
+        visible: true
+      })
+
       const map = new Map({
         controls: defaults().extend([new ScaleLine({
           units: 'metric',
@@ -52,14 +78,7 @@ export class MapService {
           zoom: DEFAULT_ZOOM,
         }),
         layers: [
-          new TileLayer({
-            source: new OSM({
-              attributions: '<a href="https://gsaukov.netlify.app/" target="_blank" style="color:blue;">Georgy Saukov</a> ' +
-                '| <a href="https://human-settlement.emergency.copernicus.eu/download.php?ds=pop" target="_blank" style="color:blue;">GHSL Data</a> ' +
-                '&copy; <a href="https://www.openstreetmap.org/copyright" target="_blank" style="color:blue;">OpenStreetMap</a> contributors',
-            }),
-            visible: true
-          }),
+          darkLayer
         ],
         target: 'ol-map',
       });
