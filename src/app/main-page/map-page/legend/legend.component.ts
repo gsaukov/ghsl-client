5b28599917fbb8c84972cf1f0f9d0ce8dfd45abd
Georgy Saukov <georgy.saukov@sap.com>
2024-05-28 20:02:22 +0200

    GHSL client legend resolution support.

--- src/app/main-page/map-page/legend/legend.component.ts
+++ src/app/main-page/map-page/legend/legend.component.ts
@@ -1,6 +1,7 @@
-import { Component, ViewChild } from '@angular/core';
+import {Component, ViewChild} from '@angular/core';
 import {ImageLayerService} from "../../../services/image-layer.service";
-import { MatDrawer } from '@angular/material/sidenav';
+import {MatDrawer} from '@angular/material/sidenav';
+import {GhslLayerResolution, TileLayerService} from "../../../services/tile-layer.service";
 
 @Component({
   selector: 'app-legend',
@@ -12,11 +13,47 @@ export class LegendComponent {
 
   isLegendOpen:boolean = false;
 
-  constructor(public imageLayerService: ImageLayerService) {
+  constructor(public imageLayerService: ImageLayerService, public tileLayerService: TileLayerService) {
   }
 
   toggle(){
     this.drawer.toggle()
     this.isLegendOpen = !this.isLegendOpen;
+  }
+
+  getAreaResolution():string {
+    if(this.tileLayerService.currentResolution === GhslLayerResolution.RES_3SS) {
+      return '10000 m'
+    } else {
+      return '1 km'
+    }
+  }
+
+  getPopulationDensity(densityRange:string):string {
+    if(this.tileLayerService.currentResolution === GhslLayerResolution.RES_3SS) {
+      switch (densityRange) {
+        case '1-10': return '0.01-0.1'
+        case '10-49': return '0.1-0.5'
+        case '50-129': return '0.5-1.3'
+        case '130-399': return '1.3-4'
+        case '400-1k': return '4-10'
+        case '1k-2k': return '10-20'
+        case '2k-3.5k': return '20-35'
+        case '3.5k-5.5k': return '35-55'
+        case '5.5k-7.5k': return '55-75'
+        case '7.5k-10k': return '75-100'
+        case '10k-12k': return '100-120'
+        case '12k-16k': return '120-160'
+        case '16k-22k': return '160-220'
+        case '22k-30k': return '220-300'
+        case '30k-50k': return '300-500'
+        case '50k-100k': return '500-1k'
+        case '100k-200k': return '1k-2k'
+        case '200k+': return '2k+'
+        default: throw Error('population density not found')
+      }
+    } else {
+      return densityRange
+    }
   }
 }
