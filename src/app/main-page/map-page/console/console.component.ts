dfed94a51bdfdecf992c130386ea5f8249a1d77d
Georgy Saukov <georgy.saukov@sap.com>
2024-06-09 19:43:14 +0200

    GHSL client console log open/close styles and default closed.

--- src/app/main-page/map-page/console/console.component.ts
+++ src/app/main-page/map-page/console/console.component.ts
@@ -12,7 +12,7 @@ export class ConsoleComponent {
 
   constructor(private imageLayerService: ImageLayerService) {
     this.loadingLayers = []
-    this.isConsoleOpen = true
+    this.isConsoleOpen = false
   }
 
   getLayers(): LoadingLayer[] {
