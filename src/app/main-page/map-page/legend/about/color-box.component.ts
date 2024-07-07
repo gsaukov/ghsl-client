de035d871bf76a53d17e0cdae2323644eade0880
Georgy Saukov <georgy.saukov@sap.com>
2024-07-07 18:48:43 +0200

    GHSL client color box height

--- src/app/main-page/map-page/legend/about/color-box.component.ts
+++ src/app/main-page/map-page/legend/about/color-box.component.ts
@@ -2,10 +2,11 @@ import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-color-box',
-  template: '<div class="color-box" style="background-color:white;">' +
-    '<div class="color-box" [style.background-color]="boxColor" [style.margin]="0"></div>' +
+  template: '<div class="color-box" [style.height]="boxHeight"  style="background-color:white;">' +
+    '<div class="color-box" [style.background-color]="boxColor" [style.height]="boxHeight" [style.margin]="0"></div>' +
     '</div>',
 })
 export class ColorBoxComponent {
   @Input() boxColor: string = '';
+  @Input() boxHeight: string = '1em';
 }
