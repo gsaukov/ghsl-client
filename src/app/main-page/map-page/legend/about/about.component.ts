932db98ddc470c270ae36c1f210f3be65dcc6961
Georgy Saukov <georgy.saukov@sap.com>
2024-07-12 17:05:41 +0200

    GHSL client About scroll indicator & control

--- src/app/main-page/map-page/legend/about/about.component.ts
+++ src/app/main-page/map-page/legend/about/about.component.ts
@@ -1,4 +1,4 @@
-import {Component, Inject} from '@angular/core';
+import { AfterViewInit, ChangeDetectorRef, Component, ElementRef, Inject, ViewChild} from '@angular/core';
 import {
   MAT_DIALOG_DATA,
   MatDialogRef
@@ -13,13 +13,50 @@ export interface DialogData {
   selector: 'app-about',
   templateUrl: 'about.component.html'
 })
-export class AboutComponent {
+export class AboutComponent implements AfterViewInit {
+  @ViewChild('scrollArea', { static: false }) scrollArea!: ElementRef;
+
+  display!: string
+  hasScrolled: boolean = false
+
   constructor(
     public dialogRef: MatDialogRef<AboutComponent>,
     @Inject(MAT_DIALOG_DATA) public data: DialogData,
+    private cdRef:ChangeDetectorRef
   ) {}
+
+  ngAfterViewInit(): void {
+    this.detectScrollIndicator()
+    this.cdRef.detectChanges()
+  }
 
   onOkClick(): void {
     this.dialogRef.close();
+  }
+
+  scrollDown(): void {
+    const target = this.scrollArea.nativeElement;
+    const scrollTop = target.scrollTop;
+    target.scrollTo({ top: scrollTop + (target.clientHeight * 0.7), behavior: 'smooth' });
+  }
+
+  detectScrollIndicator(): void {
+    if(!this.hasScrolled) {
+      const target = this.scrollArea.nativeElement
+      if (target.scrollHeight - target.scrollTop - target.clientHeight <= 40) {
+        this.hideScrollIndicator();
+      } else {
+        this.showScrollIndicator();
+      }
+    }
+  }
+
+  hideScrollIndicator(): void {
+    this.hasScrolled = true;
+    this.display ='none';
+  }
+
+  showScrollIndicator(): void {
+    this.display ='block';
   }
 }
