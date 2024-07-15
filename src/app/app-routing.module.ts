fb17d274adfdcd14a31e7f468f6d7aaa322763ef
Georgy Saukov <georgy.saukov@sap.com>
2024-07-15 22:35:45 +0200

    GHSL client about routing adjusted

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,19 @@ import {NgModule} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
 import {MainPageComponent} from "./main-page/main-page.component";
 import {MapPageComponent} from "./main-page/map-page/map-page.component";
-import {AboutComponent} from "./main-page/map-page/legend/about/about.component";
+import {AboutComponent} from "./main-page/map-page/about/about.component";
+import {DialogService} from "./services/dialog.service";
+import { inject } from '@angular/core'
 
 const routes: Routes = [
   {
     path: '', component: MainPageComponent, children: [
-      {path: '', component: MapPageComponent},
-      {path: 'about', component: AboutComponent},
+      {path: '', component: MapPageComponent, children: [
+          {path: 'about', component: AboutComponent, resolve:
+              {about: () => inject(DialogService).openAboutDialog()},
+          },
+        ]
+      },
     ]
   },
   {path: '**', redirectTo: '', pathMatch: 'full'}, //page not found redirect to main
