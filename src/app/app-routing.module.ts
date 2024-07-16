37072c34ae59cf67968b1fdafb8b4f5ca34e7529
Georgy Saukov <georgy.saukov@sap.com>
2024-07-16 13:58:23 +0200

    GHSL client empty component.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,15 +2,15 @@ import {NgModule} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
 import {MainPageComponent} from "./main-page/main-page.component";
 import {MapPageComponent} from "./main-page/map-page/map-page.component";
-import {AboutComponent} from "./main-page/map-page/about/about.component";
 import {DialogService} from "./services/dialog.service";
 import { inject } from '@angular/core'
+import {EmptyComponent} from "./main-page/map-page/about/empty.component";
 
 const routes: Routes = [
   {
     path: '', component: MainPageComponent, children: [
       {path: '', component: MapPageComponent, children: [
-          {path: 'about', component: AboutComponent, resolve:
+          {path: 'about', component: EmptyComponent, resolve:
               {about: () => inject(DialogService).openAboutDialog()},
           },
         ]
